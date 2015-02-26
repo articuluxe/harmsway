1ce4a9ec8c5e7a0acd08d1d8911f95b7d77f5fb0
Dan Harms <enniomore@icloud.com>
2015-02-26 13:43:22 -0600

    fix mode typo

--- .emacs.d/etc/templates/template.cpp
+++ .emacs.d/etc/templates/template.cpp
@@ -1,9 +1,9 @@
-// -*- Mode: c++-mode -*-
+// -*- Mode: c++ -*-
 // ${1:`(file-name-nondirectory (buffer-file-name))`} --- ${2:Short description}
 // Copyright (C) `(format-time-string "%Y")`  `(user-full-name)`
 // Author: `(user-full-name)` <`(user-login-name)`>
 // Created: `(today)`
-// Modified Time-stamp: <2015-02-26 13:33:54 dharms>
+// Modified Time-stamp: <2015-02-26 13:42:01 dharms>
 
 // Keywords:
 
