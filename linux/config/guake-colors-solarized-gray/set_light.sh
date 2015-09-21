64b948c15bc17ec1952400c3f893699fc0cc473e
Dan Harms <dan.harms@xrtrading.com>
2015-09-21 16:40:09 -0500

    harmsway: add os-specific directories

---
+++ linux/config/guake-colors-solarized-gray/set_light.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+default='solarized'
+dir=`dirname $0`
+
+if [ -d $dir/colors/$1 ]; then
+  theme=$1
+else
+  echo "Theme $1 not found, using $default"
+  theme=$default
+fi
+
+if [ "$#" -ne 1 ]; then
+  theme=$default
+fi
+
+PROFILE=${1:-Default}
+
+gconftool-2 -s -t string /apps/guake/style/background/color `cat $dir/colors/$theme/base3`
+gconftool-2 -s -t string /apps/guake/style/font/color `cat $dir/colors/$theme/base00`
+gconftool-2 -s -t string /apps/guake/style/font/palette `cat $dir/colors/$theme/palette`
