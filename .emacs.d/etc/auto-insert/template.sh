21743f1a9ae5a4cb6e6bf13ed0f1f1255e248376
Dan Harms <dan.harms@xrtrading.com>
2015-09-25 08:12:22 -0500

    Remove empty line from file templates

--- .emacs.d/etc/auto-insert/template.sh
+++ .emacs.d/etc/auto-insert/template.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env sh
-
 # -*- Mode: sh -*-
 # ${1:`(file-name-nondirectory (buffer-file-name))`} --- ${2:Short description}
 # Copyright (C) `(format-time-string "%Y")`  `(user-full-name)` (`(user-login-name)`)
 # Author: `(user-full-name)` <`user-mail-address`>
 # Created: `(insert-today)`
 # Version: ${3:1.0}
-# Modified Time-stamp: <2015-02-26 13:20:30 dharms>
-# Modified by:
+# Modified Time-stamp: <2015-09-24 09:48:17 dan.harms>
+# Modified by: Dan Harms
 # Keywords: $4
 
 $0
