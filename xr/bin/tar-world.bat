051d9f70dbcea7169859f2d939f51d16705d5e09
Dan Harms <dan.harms@xrtrading.com>
2016-01-13 17:12:45 -0600

    config modifications for xr (stage 1)

--- xr/bin/tar-world.bat
+++ xr/bin/tar-world.bat
@@ -1,18 +1,20 @@
+@echo off
 rem -*- Mode: dos -*-
 rem tar-world.bat --- tar up the world (xr style)
-rem Copyright (C) 2015  Dan Harms (dan.harms)
+rem Copyright (C) 2015, 2016  Dan Harms (dan.harms)
 rem Author: Dan Harms <dan.harms@xrtrading.com>
 rem Created: Thursday, May 21, 2015
 rem Version: 1.0
-rem Modified Time-stamp: <2015-10-16 12:12:44 dan.harms>
+rem Modified Time-stamp: <2016-01-13 16:55:15 dan.harms>
 rem Modified by: Dan Harms
 rem Keywords: tar whole world
 
-@echo off
 setlocal
 setlocal enabledelayedexpansion
 
 set tar=c:\msys\1.0\bin\tar
+set uname=c:\msys\1.0\bin\uname
+set hostname=c:\MinGW\msys\1.0\bin\hostname
 
 set verbose=
 if not %1. == . (
@@ -22,6 +24,11 @@ if not %1. == . (
 set curr_dir=%cd%
 set script_dir=%~dp0
 set dest=xr-world.tar
+
+for /f "delims=" %%i in ('%uname%') do set os=%%i
+echo os is %os%
+for /f "delims=" %%i in ('%hostname%') do set host=%%i
+echo host is %host%
 
 echo Generating %curr_dir%\%dest%...
 
@@ -33,7 +40,8 @@ if exist %dest% (
 %tar% u%verbose%f %dest% --transform=s/scripts/bin/ scripts
 %tar% u%verbose%f %dest% --transform=s$bash/$$ bash
 %tar% u%verbose%f %dest% --transform=s$tcsh/$$ tcsh
-%tar% u%verbose%f %dest% --transform=s$xr/$$ xr
+%tar% u%verbose%f %dest% --transform=s$%os%/$$ %os%
+%tar% u%verbose%f %dest% --transform=s$%host%/$$ %host%
 
 echo ...done generating %dest%
 
