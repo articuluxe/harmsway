a44f82e8f9c52ef97c22edf463408a366a39816c
Dan Harms <danielrharms@gmail.com>
2016-01-31 23:26:40 -0600

    save output of emacscomp.sh

--- scripts/emacscomp.sh
+++ scripts/emacscomp.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env sh
 # -*- Mode: sh -*-
 # emacscomp.sh --- byte-compile emacs lisp files
-# Copyright (C) 2015  Dan Harms (dan.harms)
+# Copyright (C) 2015, 2016  Dan Harms (dan.harms)
 # Author: Dan Harms <dan.harms@xrtrading.com>
 # Created: Tuesday, June 16, 2015
 # Version: 1.0
-# Modified Time-stamp: <2015-07-25 13:12:08 dharms>
+# Modified Time-stamp: <2016-01-31 17:49:42 dharms>
 # Keywords: emacs configuration
 
 emacs=$EDITOR
 dir=$(pwd)
 user=$USER
+date=$(date '+%F_%T' | tr ':' '-')
 
 if [ $# -gt 0 ] ; then
    dir="$1"
@@ -20,6 +21,10 @@ fi
 cmd="(byte-recompile-directory \"$dir\" 0 t)"
 echo "Compiling emacs files in directory $dir..."
 
-$emacs --batch -u $user --eval "$cmd"
+$emacs --batch -u $user --eval "$cmd" &> emacs-install-log-$date.log
+#Done (Total of 292 files compiled, 2 failed in 33 directories)
+grep -i 'error' emacs-install-log-$date.log
+grep -e '^Done' emacs-install-log-$date.log
+gzip emacs-install-log-$date.log
 
 # emacscomp.sh ends here
