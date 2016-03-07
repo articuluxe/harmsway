3da8a9c5eea8bf20eddd40bd8ecf4f88bf07ade9
Dan Harms <dan.harms@xrtrading.com>
2016-03-07 09:07:01 -0600

    add goto-line for mock-mode

--- .emacs.d/modes/xr-mock.el
+++ .emacs.d/modes/xr-mock.el
@@ -2,13 +2,17 @@
 ;; xr-mock.el --- major mode to view mock scripts
 ;; Author: Dan Harms <dan.harms@xrtrading.com>
 ;; Created: Wednesday, June 10, 2015
-;; Version: 1.1
-;; Modified Time-stamp: <2016-02-15 08:45:33 dan.harms>
+;; Version: 1.2
+;; Modified Time-stamp: <2016-03-07 09:06:48 dan.harms>
 ;; Keywords: mock script
 
 ;; Code:
 
 (defgroup mock-mode nil "*mock script mode" :group 'mock)
+
+(defvar mock-mode-map
+  (let ((map (make-sparse-keymap)))
+    map) "Keymap for mock-mode")
 
 (defun mock-mode()
   "Major mode for browsing mock scripts."
@@ -59,6 +63,8 @@
         ("=\\<en\\(d\\)\\_>" (1 "> b"))
         ("\\_<end_commen\\(t\\)\\_>" (1 "> b"))
         ))
+  (use-local-map mock-mode-map)
+  (define-key mock-mode-map "\M-sg" 'mock-goto-line)
   (setq comment-start "#" comment-end "")
   (subword-mode 1)
   (run-hooks 'mock-mode-hook)
@@ -72,6 +78,15 @@
  'ff-special-constructs
  '("^\\(?:@@\\)?\\(include\\|sourceFiles\\|scriptfile\\)\\s-*=\\s-*\\(.*\\)"
    lambda nil (buffer-substring (match-beginning 2) (match-end 2))) t)
+
+(defun mock-goto-line (n)
+  "Go to a line in a mock script, exclusive of continuation lines."
+  (interactive "nGoto line: ")
+  (goto-char (point-min))
+  (while (> n 1)
+    (unless (search-forward-regexp "\\\\$" (line-end-position) t)
+      (setq n (- n 1)))
+    (forward-line)))
 
 (provide 'xr-mock)
 
