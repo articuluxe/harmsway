a4b0ce4fa28668354bd242f74d35d15af5ded240
Dan Harms <danielrharms@gmail.com>
2016-03-14 17:21:56 -0500

    Initial updates for new server at work (yay!)

---
+++ host/chihq-dev-02d/bin/untar-world.sh
@@ -0,0 +1,87 @@
+#!/usr/bin/env sh
+# -*- Mode: sh -*-
+# untar-world.sh --- untar important files
+# Copyright (C) 2015, 2016  Dan Harms (dan.harms)
+# Author: Dan Harms <dan.harms@xrtrading.com>
+# Created: Monday, May 18, 2015
+# Version: 1.0
+# Modified Time-stamp: <2016-01-23 10:07:27 dharms>
+# Modified by: Dan Harms
+# Keywords: configuration
+
+tar=$TAR
+manifest=.bk_manifest
+backup=emacs_bk.tar
+os=$(uname)
+host=$(hostname -s)
+site=talbot
+input=
+
+function backup_file
+{
+   if [ -f .emacs.d/$1 ] ; then
+      echo Backing up $1
+      $tar -rvf $backup .emacs.d/$1
+   fi
+}
+
+if [ -z "$tar" ]; then
+   tar=$(which tar)
+   echo "Using $tar"
+fi
+if [ -z "$tar" ]; then
+   echo "! no tar available; quitting"
+   exit 1
+fi
+if [ $# -gt 0 ] ; then
+   input=$1
+   shift
+fi
+
+if [ "x$input" = "x" ] ; then
+   echo "Error: need an input file."
+   exit
+fi
+
+date=$(date '+%F_%T' | tr ':' '-')
+
+pushd ~
+
+# there's an existing .emacs.d
+$tar --overwrite -xpf $input .emacs.d/$manifest
+if [ -d .emacs.d ] && [ -f .emacs.d/$manifest ] ; then
+   rm -f $backup
+   files=(`cat .emacs.d/$manifest`)
+   numfiles=${#files[*]}
+   i=0
+   while [ $i -lt $numfiles ]
+   do
+      backup_file ${files[$i]}
+      i=$(( $i+1 ))
+   done
+   # backup for posterity
+   $tar czf .emacs.d.bk_$date.tgz --force-local .emacs.d
+   # restore interesting files
+   rm -rf .emacs.d
+   mkdir .emacs.d
+   if [ -r $backup ] ; then
+      $tar -xpf $backup
+      rm -f $backup
+   fi
+fi
+
+echo About to unpack $input...
+$tar --overwrite -xpvf $input
+
+# remove intermediate directories, if empty
+rmdir --ignore-fail-on-non-empty -p bash tcsh os/$os site/$site host/$host
+# protect .netrc
+if [ -f .netrc ] ; then
+   chmod 600 .netrc
+fi
+# and byte-compile emacs
+emacscomp.sh .emacs.d
+
+popd
+
+# untar-world.sh ends here
