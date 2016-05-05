8c6398cb99af4771b8510309262419b93c8382e5
Dan Harms <danielrharms@gmail.com>
2016-05-05 10:29:21 -0500

    add fileselect.sh

---
+++ scripts/fileselect.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env sh
+# -*- Mode: sh -*-
+# fileselect.sh --- select a file or dir from disk
+# Copyright (C) 2016  Dan Harms (dharms)
+# Author: Dan Harms <danielrharms@gmail.com>
+# Created: Thursday, May  5, 2016
+# Version: 1.0
+# Modified Time-stamp: <2016-05-05 10:26:58 dharms>
+# Modified by: Dan Harms
+# Keywords: bash script
+
+dir=${1:-$(pwd)}
+prompt=${2:-"Select a file or directory: "}
+# construct array of files in dir
+dirs=("$dir"/*)
+# loop through, replace with basename
+for e in "${dirs[@]}"
+do
+   dirsbase=( ${dirsbase[@]} $(basename $e) )
+done
+# concatenate into space-separated string
+opts=$(echo "${dirsbase[*]}")
+
+PS3="$prompt"
+select opt in $opts "Quit"; do
+   if [ "$opt" == "Quit" ]; then
+      exit
+   elif ((REPLY > 0)); then
+      if [ ! -z $opt ]; then
+         echo $opt
+         exit
+      fi
+   fi
+done
+
+# edit ends here
