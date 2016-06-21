758e132c2fbf3f5622c4ba64b060196bdaf6dd67
Dan Harms <danielrharms@gmail.com>
2016-06-21 08:27:07 -0500

    outrespace: enhance anonymous usage, change enclosing

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,7 +3,7 @@
 ;; Author: Dan Harms <danielrharms@gmail.com>
 ;; Created: Friday, February 27, 2015
 ;; Version: 1.0
-;; Modified Time-stamp: <2016-06-14 17:54:45 dharms>
+;; Modified Time-stamp: <2016-06-17 17:27:06 dharms>
 ;; Modified by: Dan Harms
 ;; Keywords:
 
@@ -321,8 +321,9 @@ Cf. `http://ergoemacs.org/emacs/emacs_CSS_colors.html'."
 (use-package
  outrespace
  :bind (("C-c z" . outrespace-mode)     ;temporary
-        ("C-c nc" . outre-wrap-namespace-region)
+        ("C-c nn" . outre-wrap-namespace-region)
         ("C-c nj" . outre-ivy-jump-to-ns)
+        ("C-c nc" . outre-change-enclosing-ns-name)
         )
  )
 
