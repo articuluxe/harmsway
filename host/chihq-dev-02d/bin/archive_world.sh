17b799a01a1aae5d36cdcc456ef94b4056845bce
Dan Harms <dan.harms@xrtrading.com>
2016-08-17 10:32:58 -0500

    add archive-world to chihq-dev-02d

---
+++ host/chihq-dev-02d/bin/archive_world.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# -*- Mode: sh -*-
+# archive_world.sh --- archive a host's vital files
+# Copyright (C) 2016  Dan Harms (dharms)
+# Author: Dan Harms <danielrharms@gmail.com>
+# Created: Tuesday, August  9, 2016
+# Version: 1.0
+# Modified Time-stamp: <2016-08-17 09:37:31 dan.harms>
+# Modified by: Dan Harms
+# Keywords: archive backup
+
+target=${1:-"~/backups"}
+
+_backup.sh ~/src/harmsway/site/xr "$target"
+
+# code ends here
