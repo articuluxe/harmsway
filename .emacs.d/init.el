97659de7ba595cb3c20ebc716318fb0ac360748a
Dan Harms <dan.harms@xrtrading.com>
2016-08-18 14:50:36 -0500

    add pcap-mode.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,7 +3,7 @@
 ;; Author: Dan Harms <danielrharms@gmail.com>
 ;; Created: Friday, February 27, 2015
 ;; Version: 1.0
-;; Modified Time-stamp: <2016-08-17 10:28:49 dan.harms>
+;; Modified Time-stamp: <2016-08-18 14:41:27 dan.harms>
 ;; Modified by: Dan Harms
 ;; Keywords:
 
@@ -2304,6 +2304,11 @@ customization."
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; nhexl-mode ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (use-package nhexl-mode :defer t)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; pcap-mode ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(use-package pcap-mode
+  :mode ("\\.pcap$" . pcap-mode)
+  )
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; python-mode ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (eval-and-compile
