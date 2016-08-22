79db5d0471047551244bf8ae7cd4bc0d6b048d2a
Dan Harms <danielrharms@gmail.com>
2016-08-22 04:57:58 -0500

    refine backed-up dirs on Portsmouth

--- host/Portsmouth/bin/archive_world.sh
+++ host/Portsmouth/bin/archive_world.sh
@@ -5,13 +5,12 @@
 # Author: Dan Harms <danielrharms@gmail.com>
 # Created: Tuesday, August  9, 2016
 # Version: 1.0
-# Modified Time-stamp: <2016-08-17 12:47:05 dan.harms>
+# Modified Time-stamp: <2016-08-22 04:57:37 dharms>
 # Modified by: Dan Harms
 # Keywords: archive backup
 
 target=${1:-~/backups}
 
 _backup.sh ~/Dropbox/notes "$target"
-_backup.sh ~/Dropbox/movies "$target"
 
 # code ends here
