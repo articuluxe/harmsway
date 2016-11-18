0d41080ae7c311a2419868e804ecced13fcd95f6
Dan Harms <dan.harms@xrtrading.com>
2016-11-18 16:41:01 -0600

    update csharp-mode, ibuffer-vc, flycheck-pos-tip

--- .emacs.d/plugins/flycheck-pos-tip.el
+++ .emacs.d/plugins/flycheck-pos-tip.el
@@ -8,8 +8,8 @@
 ;; Maintainer: Sebastian Wiesner <swiesner@lunaryorn.com>
 ;; URL: https://github.com/flycheck/flycheck-pos-tip
 ;; Keywords: tools, convenience
-;; Version: 0.3-cvs
-;; Package-Requires: ((dash "2.12") (flycheck "0.22") (pos-tip "0.4.6"))
+;; Version: 0.4-cvs
+;; Package-Requires: ((emacs "24.1") (flycheck "0.22") (pos-tip "0.4.6"))
 
 ;; This file is not part of GNU Emacs.
 
@@ -37,7 +37,6 @@
 
 ;;; Code:
 
-(require 'dash)
 (require 'flycheck)
 (require 'pos-tip)
 
@@ -66,10 +65,10 @@ messages on TTY frames if `flycheck-pos-tip-mode' is active."
 (defun flycheck-pos-tip-error-messages (errors)
   "Display ERRORS, using a graphical tooltip on GUI frames."
   (when errors
-    (let ((message (mapconcat #'flycheck-error-format-message-and-id
-                              errors "\n\n"))
-          (line-height (car (window-line-height))))
-      (if (display-graphic-p)
+    (if (display-graphic-p)
+        (let ((message (mapconcat #'flycheck-error-format-message-and-id
+                                  errors "\n\n"))
+              (line-height (car (window-line-height))))
           (pos-tip-show message nil nil nil flycheck-pos-tip-timeout
                         nil nil
                         ;; Add a little offset to the tooltip to move it away
@@ -77,12 +76,14 @@ messages on TTY frames if `flycheck-pos-tip-mode' is active."
                         ;; explicitly take the line height into account because
                         ;; pos-tip computes the offset from the top of the line
                         ;; apparently.
-                        nil (and line-height (+ line-height 5)))
-        (funcall flycheck-pos-tip-display-errors-tty-function errors)))))
+                        nil (and line-height (+ line-height 5))))
+      (funcall flycheck-pos-tip-display-errors-tty-function errors))))
 
 (defun flycheck-pos-tip-hide-messages ()
   "Hide messages currently being shown if any."
-  (pos-tip-hide))
+  (if (display-graphic-p)
+      (pos-tip-hide)
+    (flycheck-hide-error-buffer)))
 
 (defvar flycheck-pos-tip-old-display-function nil
   "The former value of `flycheck-display-errors-function'.")
@@ -106,18 +107,28 @@ TTY frames."
   :global t
   :group 'flycheck
   (let ((hooks '(post-command-hook focus-out-hook)))
-    (if flycheck-pos-tip-mode
-        (progn
-          (setq flycheck-pos-tip-old-display-function
-                flycheck-display-errors-function
-                flycheck-display-errors-function
-                #'flycheck-pos-tip-error-messages)
-          (dolist (hook hooks)
-            (add-hook hook #'flycheck-pos-tip-hide-messages)))
-      (setq flycheck-display-errors-function
-            flycheck-pos-tip-old-display-function)
+    (cond
+     ;; Use our display function and remember the old one but only if we haven't
+     ;; yet configured it, to avoid activating twice.
+     ((and flycheck-pos-tip-mode
+           (not (eq flycheck-display-errors-function
+                    #'flycheck-pos-tip-error-messages)))
+      (setq flycheck-pos-tip-old-display-function
+            flycheck-display-errors-function
+            flycheck-display-errors-function
+            #'flycheck-pos-tip-error-messages)
       (dolist (hook hooks)
-        (remove-hook hook 'flycheck-pos-tip-hide-messages)))))
+        (add-hook hook #'flycheck-pos-tip-hide-messages)))
+     ;; Reset the display function and remove ourselves from all hooks but only
+     ;; if the mode is still active.
+     ((and (not flycheck-pos-tip-mode)
+           (eq flycheck-display-errors-function
+               #'flycheck-pos-tip-error-messages))
+      (setq flycheck-display-errors-function
+            flycheck-pos-tip-old-display-function
+            flycheck-pos-tip-old-display-function nil)
+      (dolist (hook hooks)
+        (remove-hook hook 'flycheck-pos-tip-hide-messages))))))
 
 (provide 'flycheck-pos-tip)
 
