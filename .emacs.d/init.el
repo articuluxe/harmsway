aecba68e9e4a52b8f69ec12ed82fda8686c2d419
Dan Harms <danielrharms@gmail.com>
2017-06-19 17:47:24 -0500

    add shortcut for proviso-open-dired

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,7 +3,7 @@
 ;; Author: Dan Harms <danielrharms@gmail.com>
 ;; Created: Friday, February 27, 2015
 ;; Version: 1.0
-;; Modified Time-stamp: <2017-06-19 14:27:37 dan.harms>
+;; Modified Time-stamp: <2017-06-19 20:22:30 dharms>
 ;; Modified by: Dan Harms
 ;; Keywords:
 
@@ -252,6 +252,7 @@ Cf. `http://ergoemacs.org/emacs/emacs_CSS_colors.html'."
   :demand t
   :bind (("C-c g" . proviso-grep)
          ("C-c t" . proviso-gentags-generate-tags)
+         ("C-c pd" . proviso-open-dired)
          ("C-c pp" . proviso-display-print-project)
          ("C-c pn" . proviso-display-echo-project-names)
          ("C-c pe" . proviso-display-projects)
