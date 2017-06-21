0ec9d20f28838f29ac381a26e10eb75a5c53a862
Dan Harms <dan.harms@xrtrading.com>
2017-06-21 16:46:44 -0500

    adjust order of options in _prune-empty-dirs.sh

--- scripts/_prune-empty-dirs.sh
+++ scripts/_prune-empty-dirs.sh
@@ -5,12 +5,12 @@
 # Author: Dan Harms <enniomore@icloud.com>
 # Created: Tuesday, June 13, 2017
 # Version: 1.0
-# Modified Time-stamp: <2017-06-13 08:50:21 dharms>
+# Modified Time-stamp: <2017-06-21 11:59:05 dan.harms>
 # Modified by: Dan Harms
 # Keywords: utils helper directory
 
 dir=${1:-$(pwd)}
 
-find "$dir" -type d -mindepth 1 -empty -delete
+find "$dir" -mindepth 1 -type d -empty -delete
 
 # code ends here
