18c6ef5b76f69f17820adbc61a730d0c1aceb5a6
Dan Harms <dan.harms@xrtrading.com>
2017-08-03 11:17:54 -0500

    add a missing keybinding for iflipb

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,7 +2,7 @@
 ;; Copyright (C) 2015-2017  Dan Harms (dharms)
 ;; Author: Dan Harms <danielrharms@gmail.com>
 ;; Created: Friday, February 27, 2015
-;; Modified Time-stamp: <2017-08-02 17:43:41 dharms>
+;; Modified Time-stamp: <2017-08-03 11:15:25 dan.harms>
 ;; Modified by: Dan Harms
 ;; Keywords:
 
@@ -625,6 +625,7 @@ line."
   iflipb
   :bind (("<C-tab>" . iflipb-next-buffer)
          ("M-' TAB" . iflipb-next-buffer)
+         ("<C-S-tab>" . iflipb-previous-buffer)
          ("<C-S-iso-lefttab>" . iflipb-previous-buffer)
          ("M-' <backtab>" . iflipb-previous-buffer)
          ))
