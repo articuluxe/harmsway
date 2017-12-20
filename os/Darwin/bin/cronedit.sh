351e5ee9f7e2fa59997373639945cc3b49c19f3f
Dan Harms <danielrharms@gmail.com>
2017-12-20 09:38:31 -0600

    Use $VISUAL rather than $EMACSX

--- os/Darwin/bin/cronedit.sh
+++ os/Darwin/bin/cronedit.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env sh
 # -*- Mode: sh -*-
 # cronedit.sh --- edit cron on darwin
-# Copyright (C) 2016  Dan Harms (dharms)
+# Copyright (C) 2016-2017  Dan Harms (dharms)
 # Author: Dan Harms <danielrharms@gmail.com>
 # Created: Thursday, August 18, 2016
 # Version: 1.0
-# Modified Time-stamp: <2016-08-18 06:36:55 dharms>
+# Modified Time-stamp: <2017-12-20 09:13:16 dharms>
 # Modified by: Dan Harms
 # Keywords: cron
 
 # usage: EDITOR=cronedit.sh crontab -e
 
-$($EMACSX -nw "$@")
+$($VISUAL -nw "$@")
 
 # code ends here
