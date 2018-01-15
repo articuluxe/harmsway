c8bd3d64fbb1241bf9aa9c061354bdff3b819fd2
Dan Harms <dan.harms@xrtrading.com>
2018-01-15 14:20:41 -0600

    add error checking into win32 emacs batch script

--- os/MINGW32_NT-6.1/bin/emacs.bat
+++ os/MINGW32_NT-6.1/bin/emacs.bat
@@ -5,12 +5,17 @@ rem Copyright (C) 2018  Dan Harms (dan.harms)
 rem Author: Dan Harms <dan.harms@xrtrading.com>
 rem Created: Friday, January 12, 2018
 rem Version: 1.0
-rem Modified Time-stamp: <2018-01-15 10:20:07 dan.harms>
+rem Modified Time-stamp: <2018-01-15 14:20:01 dan.harms>
 rem Modified by: Dan Harms
 rem Keywords: tools win32
 
 setlocal
 setlocal enabledelayedexpansion
+
+if "%EMACS%". == . (
+    echo "EMACS undefined, aborting."
+    exit /b
+)
 
 %EMACS%\bin\emacs.exe -nw %*
 
