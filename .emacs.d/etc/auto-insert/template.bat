cf2b0464953f20f7c88d511c8e7ba0067732c0c5
Dan Harms <dan.harms@xrtrading.com>
2018-01-15 11:06:47 -0600

    forgot a dos-mode change; use entire path (again) on win32

--- .emacs.d/etc/auto-insert/template.bat
+++ .emacs.d/etc/auto-insert/template.bat
@@ -1,14 +1,14 @@
-rem -*- Mode: dos -*-
+@echo off
+rem -*- Mode: bat -*-
 rem ${1:`(file-name-nondirectory (or buffer-file-name (buffer-name)))`} --- ${2:Short description}
 rem Copyright (C) `(format-time-string "%Y")`  `(user-full-name)` (`(user-login-name)`)
 rem Author: `(user-full-name)` <`user-mail-address`>
 rem Created: `(insert-today)`
 rem Version: ${3:1.0}
-rem Modified Time-stamp: <2015-12-02 09:26:16 dan.harms>
+rem Modified Time-stamp: <2018-01-15 10:20:58 dan.harms>
 rem Modified by: Dan Harms
 rem Keywords: $4
 
-@echo off
 setlocal
 setlocal enabledelayedexpansion
 
