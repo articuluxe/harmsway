08d34bf86b8bca67215005e46b5c104a1c799424
Dan Harms <dan.harms@xrtrading.com>
2018-01-19 14:43:44 -0600

    add distinguished theme

--- .emacs.d/custom/custom-themes.el
+++ .emacs.d/custom/custom-themes.el
@@ -2,7 +2,7 @@
 ;; Copyright (C) 2016-2018  Dan Harms (dan.harms)
 ;; Author: Dan Harms <dan.harms@xrtrading.com>
 ;; Created: Wednesday, December 21, 2016
-;; Modified Time-stamp: <2018-01-17 09:48:29 dan.harms>
+;; Modified Time-stamp: <2018-01-19 14:49:29 dan.harms>
 ;; Modified by: Dan Harms
 ;; Keywords: themes colors
 
@@ -95,6 +95,8 @@
            (custom-theme-set-faces sym '(mode-line-inactive ((t (:inherit header-line
                                                                           :foreground "gray14"
                                                                           :background "gray6"))))))
+          ((eq sym 'distinguished)
+           (my/make-comment-delimiter-face-same-as-comment-face sym))
           ((eq sym 'granger)
            (my/make-comment-delimiter-face-same-as-comment-face sym))
           ((eq sym 'hober2)
