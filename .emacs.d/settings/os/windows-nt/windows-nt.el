3ae28b3fd156fe74ae6ead38d7f4197ca471c4c6
Dan Harms <dan.harms@xrtrading.com>
2018-02-12 10:19:33 -0600

    don't use bash for explicit-shell-file-name on windows host

--- .emacs.d/settings/os/windows-nt/windows-nt.el
+++ .emacs.d/settings/os/windows-nt/windows-nt.el
@@ -2,7 +2,7 @@
 ;; Copyright (C) 2015-2018  Dan Harms (dharms)
 ;; Author: Dan Harms <danielrharms@gmail.com>
 ;; Created: Saturday, February 28, 2015
-;; Modified Time-stamp: <2018-02-14 06:04:00 dan.harms>
+;; Modified Time-stamp: <2018-02-14 06:05:44 dan.harms>
 ;; Modified by: Dan Harms
 ;; Keywords:
 
