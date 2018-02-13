57a2ede6336f7b84ab683642f66ac5fcb1843887
Dan Harms <dan.harms@xrtrading.com>
2018-02-13 11:54:35 -0600

    disable shell-file-name on win32 for now

--- .emacs.d/settings/os/windows-nt/windows-nt.el
+++ .emacs.d/settings/os/windows-nt/windows-nt.el
@@ -2,7 +2,7 @@
 ;; Copyright (C) 2015-2018  Dan Harms (dharms)
 ;; Author: Dan Harms <danielrharms@gmail.com>
 ;; Created: Saturday, February 28, 2015
-;; Modified Time-stamp: <2018-02-14 06:02:56 dan.harms>
+;; Modified Time-stamp: <2018-02-14 06:04:00 dan.harms>
 ;; Modified by: Dan Harms
 ;; Keywords:
 
@@ -43,9 +43,9 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ssh-agency ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;(require 'ssh-agency)
 
-(setq shell-file-name "sh")             ;finds msys
-(setq explicit-shell-file-name
-      (concat (getenv "MSYS") "\\bin\\bash.exe"))
+;(setq shell-file-name "sh")             ;finds msys
+;; (setq explicit-shell-file-name
+;;       (concat (getenv "MSYS") "\\bin\\bash.exe"))
 
 (setq-default comint-process-echoes t)
 (setq w32-get-true-file-attributes nil)
