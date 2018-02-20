6ed258ea1a5cc43e55ddf4d0304c8597f4facd85
Dan Harms <dan.harms@xrtrading.com>
2018-02-20 12:30:12 -0600

    adjust tangotango theme's magit-section-highlight face

--- .emacs.d/custom/custom-themes.el
+++ .emacs.d/custom/custom-themes.el
@@ -2,7 +2,7 @@
 ;; Copyright (C) 2016-2018  Dan Harms (dan.harms)
 ;; Author: Dan Harms <dan.harms@xrtrading.com>
 ;; Created: Wednesday, December 21, 2016
-;; Modified Time-stamp: <2018-02-08 06:31:16 dharms>
+;; Modified Time-stamp: <2018-02-20 12:28:22 dan.harms>
 ;; Modified by: Dan Harms
 ;; Keywords: themes colors
 
@@ -126,6 +126,8 @@
            (custom-theme-set-faces sym '(cursor ((t (:background "yellow3"))))))
           ((eq sym 'subatomic)
            (my/make-comment-delimiter-face-same-as-comment-face sym))
+          ((eq sym 'tangotango)
+           (custom-theme-set-faces sym '(magit-section-highlight ((t (:background "grey25"))))))
           ((eq sym 'tao-yang)
            (my/make-comment-delimiter-face-same-as-comment-face sym))
           ((eq sym 'tao-yin)
