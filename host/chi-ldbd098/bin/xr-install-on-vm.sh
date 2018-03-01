5e343ed05e4b17b8e23f4287ca8d27e711974387
Dan Harms <dan.harms@xrtrading.com>
2018-03-01 12:58:17 -0600

    add a helper script to install world on a vm

---
+++ host/chi-ldbd098/bin/xr-install-on-vm.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# -*- Mode: sh -*-
+# xr-install-on-vm.sh --- install xr-world on vm
+# Copyright (C) 2018  Dan Harms (dan.harms)
+# Author: Dan Harms <dan.harms@xrtrading.com>
+# Created: Thursday, March  1, 2018
+# Version: 1.0
+# Modified Time-stamp: <2018-03-01 11:00:44 dan.harms>
+# Modified by: Dan Harms
+# Keywords: xr config
+
+ssh vm 'xr-install-world.sh'
+
+# code ends here
