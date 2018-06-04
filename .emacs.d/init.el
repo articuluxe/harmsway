a78c76051db5844cb8a6dc4f0adb380bb54a2e2d
Dan Harms <dan.harms@xrtrading.com>
2018-06-04 13:39:49 -0500

    disable persp-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,7 +2,7 @@
 ;; Copyright (C) 2015-2018  Dan Harms (dharms)
 ;; Author: Dan Harms <danielrharms@gmail.com>
 ;; Created: Friday, February 27, 2015
-;; Modified Time-stamp: <2018-06-04 13:25:12 dan.harms>
+;; Modified Time-stamp: <2018-06-04 13:39:30 dan.harms>
 ;; Modified by: Dan Harms
 ;; Keywords:
 
@@ -2118,6 +2118,7 @@ Only one letter is shown, the first that applies."
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; perspective ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (use-package perspective
+  :disabled
   :demand t
   :config (persp-mode))
 
