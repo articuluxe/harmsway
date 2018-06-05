298c676bdc91fda2ad302f68069c4b8b3a9af286
Dan Harms <danielrharms@gmail.com>
2018-06-04 21:09:00 -0500

    minor updates to vc-msg, perspective

--- .emacs.d/plugins/magit/lisp/magit-pkg.el
+++ .emacs.d/plugins/magit/lisp/magit-pkg.el
@@ -1,10 +1,10 @@
-(define-package "magit" "2.12.0"
+(define-package "magit" "2.13.0"
   "A Git porcelain inside Emacs."
   '((emacs "24.4")
     (async "20180527")
     (dash "20180413")
     (ghub "20180417")
-    (git-commit "20180411")
+    (git-commit "20180602")
     (let-alist "1.0.5")
     (magit-popup "20180509")
     (with-editor "20180414")))
