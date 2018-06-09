fce365b3d0b81ac8c672cc367fd42dc810512605
Dan Harms <danielrharms@gmail.com>
2018-06-09 08:25:04 -0500

    adjust cursor for busybee theme

--- .emacs.d/custom/custom-themes.el
+++ .emacs.d/custom/custom-themes.el
@@ -2,7 +2,7 @@
 ;; Copyright (C) 2016-2018  Dan Harms (dan.harms)
 ;; Author: Dan Harms <dan.harms@xrtrading.com>
 ;; Created: Wednesday, December 21, 2016
-;; Modified Time-stamp: <2018-06-04 14:38:47 dan.harms>
+;; Modified Time-stamp: <2018-06-09 08:21:57 dharms>
 ;; Modified by: Dan Harms
 ;; Keywords: themes colors
 
@@ -88,6 +88,8 @@
            (my/make-comment-delimiter-face-same-as-comment-face sym))
           ((eq sym 'brin)
            (my/make-comment-delimiter-face-same-as-comment-face sym))
+          ((eq sym 'busybee)
+           (custom-theme-set-faces sym '(cursor ((t (:background "LightYellow"))))))
           ((eq sym 'deep-blue)
            (custom-theme-set-faces sym '(show-paren-match-face ((t (:bold t :background "coral")))))
            (custom-theme-set-faces sym '(show-paren-mismatch-face ((t (:bold t :background "VioletRed4"))))))
