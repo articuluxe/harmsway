6696a3987f289fd688b173d29c67c1645ed77ac7
Dan Harms <dan.harms@xrtrading.com>
2018-06-26 12:23:26 -0500

    add termbright theme

--- .emacs.d/custom/custom-themes.el
+++ .emacs.d/custom/custom-themes.el
@@ -2,7 +2,7 @@
 ;; Copyright (C) 2016-2018  Dan Harms (dan.harms)
 ;; Author: Dan Harms <dan.harms@xrtrading.com>
 ;; Created: Wednesday, December 21, 2016
-;; Modified Time-stamp: <2018-06-25 10:54:42 dan.harms>
+;; Modified Time-stamp: <2018-06-26 12:31:19 dan.harms>
 ;; Modified by: Dan Harms
 ;; Keywords: themes colors
 
@@ -105,6 +105,10 @@
                                                                           :background "gray6")))))
            (custom-theme-set-faces sym '(magit-section-highlight ((t (:background "grey23"))))))
           ((eq sym 'distinguished)
+           (my/make-comment-delimiter-face-same-as-comment-face sym))
+          ((eq sym 'eltbus)
+           (my/make-comment-delimiter-face-same-as-comment-face sym))
+          ((eq sym 'evenhold)
            (my/make-comment-delimiter-face-same-as-comment-face sym))
           ((eq sym 'granger)
            (my/make-comment-delimiter-face-same-as-comment-face sym))
