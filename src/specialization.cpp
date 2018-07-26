7f5d197d48a866eb4ee0f5404de620bd33237b4b
Dan Harms <dan.harms@xrtrading.com>
2018-07-26 09:22:36 -0500

    add specialization.cpp test file

---
+++ src/specialization.cpp
@@ -0,0 +1,28 @@
+#include <typeinfo>
+
+template <typename T>
+struct trait;
+
+template<>
+struct trait<bool>
+{
+    using type = bool;
+};
+
+struct not_specialized {};
+
+template <typename, typename = void>
+struct has_trait_helper : std::false_type {};
+
+template <typename T>
+//struct has_trait_helper<T, decltype(trait<T>::type)> : std::true_type {};
+struct has_trait_helper<T, typename trait<T>::type> : std::true_type {};
+
+template <typename T>
+struct has_trait : has_trait_helper<T, T> {};
+
+int main()
+{
+    std::cout << "Bool:" << has_trait<bool>::value << std::endl;
+    std::cout << "Other:" << has_trait<not_specialized>::value << std::endl;
+}
