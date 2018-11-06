4a01c55e656d4fad9fc8c737b35763534025969d
Dan Harms <dan.harms@xrtrading.com>
2018-11-06 12:50:54 -0600

    kitty-gen.sh first draft

---
+++ scripts/kitty-gen.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+# -*- Mode: sh -*-
+# kitty-gen.sh --- generate a kitty config file
+# Copyright (C) 2018  Dan Harms (dan.harms)
+# Author: Dan Harms <dan.harms@xrtrading.com>
+# Created: Tuesday, November  6, 2018
+# Version: 1.0
+# Modified Time-stamp: <2018-11-06 12:49:04 dan.harms>
+# Modified by: Dan Harms
+# Keywords: tools
+
+src=$1
+sess=$2
+base=$(echo $(basename "$src") | sed -e 's/.\{1,2\}. \(.*\).reg/\1/')
+dst="$sess"-$(echo "$base" | sed -e 's/ //g')
+
+[ -f "$sess".template ] || ( echo "$sess.template missing!" && exit 1 )
+
+cp "$sess".template "$dst"
+sed -n 's/"Colour\(.*\)"="\(.*\)"/Colour\1\\\2\\/ p' < "$src" >> "$dst"
+
+echo "Created $dst"
+
+# sed -nf reg-to-kitty.sed
+
+# code ends here
