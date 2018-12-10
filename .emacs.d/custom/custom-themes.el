6cbb55c551fb148447aac22f7b0d4411bf42c95d
Dan Harms <dan.harms@xrtrading.com>
2018-12-10 14:19:16 -0600

    customize paren-match for underwater theme

--- .emacs.d/custom/custom-themes.el
+++ .emacs.d/custom/custom-themes.el
@@ -2,7 +2,7 @@
 ;; Copyright (C) 2016-2018  Dan Harms (dan.harms)
 ;; Author: Dan Harms <dan.harms@xrtrading.com>
 ;; Created: Wednesday, December 21, 2016
-;; Modified Time-stamp: <2018-12-03 11:30:57 dan.harms>
+;; Modified Time-stamp: <2018-12-10 14:21:26 dan.harms>
 ;; Modified by: Dan Harms
 ;; Keywords: themes colors
 
@@ -171,6 +171,8 @@
            (harmsway/make-comment-delimiter-face-same-as-comment-face sym))
           ((eq sym 'tao-yin)
            (harmsway/make-comment-delimiter-face-same-as-comment-face sym))
+          ((eq sym 'underwater)
+           (custom-theme-set-faces sym '(show-paren-match ((t (:background "royal blue"))))))
           ((eq sym 'warm-night)
            (custom-theme-set-faces sym '(cursor ((t (:background "yellow"))))))
           ((eq sym 'weyland-yutani)
