666776d020cb240058c632a49d0bfcaeadc7df57
Dan Harms <dan.harms@xrtrading.com>
2019-07-08 11:59:01 -0500

    adjust github-modern theme's paren-match faces

--- .emacs.d/custom/custom-themes.el
+++ .emacs.d/custom/custom-themes.el
@@ -2,7 +2,7 @@
 ;; Copyright (C) 2016-2019  Dan Harms (dan.harms)
 ;; Author: Dan Harms <dan.harms@xrtrading.com>
 ;; Created: Wednesday, December 21, 2016
-;; Modified Time-stamp: <2019-07-03 12:46:19 dan.harms>
+;; Modified Time-stamp: <2019-07-08 11:55:51 dan.harms>
 ;; Modified by: Dan Harms
 ;; Keywords: themes colors
 
@@ -122,6 +122,9 @@
            (custom-theme-set-faces sym '(iedit-occurrence ((t (:inherit region))))))
           ((eq sym 'foggy-night)
            (custom-theme-set-faces sym '(cursor ((t (:background "yellow"))))))
+          ((eq sym 'github-modern)
+           (custom-theme-set-faces sym '(show-paren-match ((t (:bold t :foreground "#d0d0d0" :background "#032f62")))))
+           (custom-theme-set-faces sym '(show-paren-mismatch ((t (:bold t :foreground "#ffffff" :background "#d73a49"))))))
           ((eq sym 'granger)
            (harmsway/make-comment-delimiter-face-same-as-comment-face sym))
           ((eq sym 'grayscale)
