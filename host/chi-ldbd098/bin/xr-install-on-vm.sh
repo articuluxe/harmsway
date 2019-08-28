50bcf8f7a06600299ac3b7a5314b7093c605612d
Dan Harms <dan.harms@xrtrading.com>
2019-08-28 10:24:39 -0500

    xr-install-on-vm creates tar file, allowing standalone execution

--- host/chi-ldbd098/bin/xr-install-on-vm.sh
+++ host/chi-ldbd098/bin/xr-install-on-vm.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env sh
 # -*- Mode: sh -*-
 # xr-install-on-vm.sh --- install xr-world on vm
-# Copyright (C) 2018  Dan Harms (dan.harms)
+# Copyright (C) 2018-2019  Dan Harms (dan.harms)
 # Author: Dan Harms <dan.harms@xrtrading.com>
 # Created: Thursday, March  1, 2018
 # Version: 1.0
-# Modified Time-stamp: <2018-03-01 11:00:44 dan.harms>
+# Modified Time-stamp: <2019-08-28 10:24:22 dan.harms>
 # Modified by: Dan Harms
 # Keywords: xr config
 
+cd ~/src/harmsway/ || exit 1
+xr-tar-world.sh
 ssh vm 'xr-install-world.sh'
 
 # code ends here
