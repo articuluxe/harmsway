7e93cf4091ab436e179373f1a162cbcbf07faf88
Dan Harms <dan.harms@xrtrading.com>
2019-09-13 09:02:20 -0500

    install ~/.proviso.d

--- host/CHIHQ-DEV84/bin/tar-world.bat
+++ host/CHIHQ-DEV84/bin/tar-world.bat
@@ -5,7 +5,7 @@ rem Copyright (C) 2015-2019  Dan Harms (dan.harms)
 rem Author: Dan Harms <dan.harms@xrtrading.com>
 rem Created: Thursday, May 21, 2015
 rem Version: 1.0
-rem Modified Time-stamp: <2019-08-21 10:22:24 dan.harms>
+rem Modified Time-stamp: <2019-09-13 09:01:55 dan.harms>
 rem Modified by: Dan Harms
 rem Keywords: tar whole world
 
@@ -46,7 +46,7 @@ echo Generating %curr_dir%\%dest%...
 if exist %dest% (
     del %dest%
 )
-%tar% c%verbose%f %dest% config doc src .gnupg .fonts .config .terminfo
+%tar% c%verbose%f %dest% config doc src .gnupg .fonts .config .terminfo .proviso.d
 %tar% u%verbose%f %dest% --exclude=*.elc .emacs.d
 %tar% u%verbose%f %dest% --transform=s$ext$.emacs.d/ext$ ext
 %tar% u%verbose%f %dest% --transform=s/scripts/bin/ scripts
