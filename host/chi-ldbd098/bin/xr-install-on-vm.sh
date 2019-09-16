3fbbe32a78d81153f451961aab186820e9f9f05e
Dan Harms <dan.harms@xrtrading.com>
2019-09-16 10:19:20 -0500

    rectify silly error in vm install script

--- host/chi-ldbd098/bin/xr-install-on-vm.sh
+++ host/chi-ldbd098/bin/xr-install-on-vm.sh
@@ -5,12 +5,13 @@
 # Author: Dan Harms <dan.harms@xrtrading.com>
 # Created: Thursday, March  1, 2018
 # Version: 1.0
-# Modified Time-stamp: <2019-08-28 10:24:22 dan.harms>
+# Modified Time-stamp: <2019-09-16 10:17:12 dan.harms>
 # Modified by: Dan Harms
 # Keywords: xr config
 
 cd ~/src/harmsway/ || exit 1
 xr-tar-world.sh
+scp -pq xr-world.tar vm:~
 ssh vm 'xr-install-world.sh'
 
 # code ends here
