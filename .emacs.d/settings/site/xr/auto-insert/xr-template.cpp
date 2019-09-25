e721bfba1376a14b2f4f32102137607b30db99fb
Dan Harms <dan.harms@xrtrading.com>
2019-09-25 12:58:35 -0500

    move xr c++ template files to private repo

--- .emacs.d/settings/site/xr/auto-insert/xr-template.cpp
+++
@@ -1,7 +0,0 @@
-// ${1:`(file-name-nondirectory (buffer-file-name))`} --- ${2:Short description}
-// Author: `(user-full-name)` <`user-mail-address`>
-// Created: `(insert-today)`
-// Modified Time-stamp: <2015-08-19 14:52:30 dan.harms>
-// Modified by: Dan Harms
-
-$0
--- .emacs.d/settings/site/xr/auto-insert/xr-template.hpp
+++
@@ -1,8 +0,0 @@
-// ${1:`(file-name-nondirectory (buffer-file-name))`} --- ${2:Short description}
-// Author: `(user-full-name)` <`user-mail-address`>
-// Created: `(insert-today)`
-// Modified Time-stamp: <2015-08-19 14:52:18 dan.harms>
-// Modified by: Dan Harms
-#pragma once
-
-$0
