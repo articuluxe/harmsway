af14aa80fd5db09f2e88367f8c9d9e2b53104ea4
Dan Harms <dan.harms@xrtrading.com>
2019-12-07 05:45:03 -0600

    update windows install due to hostname (part 2)

--- os/windows32/bin/setenv.bat
+++ os/windows32/bin/setenv.bat
@@ -5,7 +5,7 @@ rem Copyright (C) 2019  Dan Harms (Dan.Harms)
 rem Author: Dan Harms <dan.harms@xrtrading.com>
 rem Created: Thursday, December  5, 2019
 rem Version: 1.0
-rem Modified Time-stamp: <2019-12-06 11:32:09 Dan.Harms>
+rem Modified Time-stamp: <2019-12-06 16:16:48 Dan.Harms>
 rem Modified by: Dan.Harms
 rem Keywords:
 
@@ -21,6 +21,7 @@ setx MSYS_DIR "C:\Program Files (x86)\GnuWin32\bin"
 setx GIT_DIR "C:\Program Files\Git"
 setx FIND_EXE "%GIT_DIR%\usr\bin\find"
 setx GREP_EXE "%GIT_DIR%\usr\bin\grep"
+setx HOSTNAME_EXE "%MSYS_DIR%\hostname"
 setx TAR_EXE "%GIT_DIR%\usr\bin\tar"
 setx PUTTY_DIR "%ChocolateyInstall%\bin"
 
