911eda7e54bfad049d5b9ca7daca55f1e6ea0362
Dan Harms <dan.harms@xrtrading.com>
2020-02-12 13:36:23 -0600

    add a script to run duti on darwin, to set file associations to Emacs

---
+++ os/Darwin/bin/duti.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env sh
+# -*- Mode: sh -*-
+# duti.sh --- duti script for os x file associations
+# Copyright (C) 2020  Dan Harms (dan.harms)
+# Author: Dan Harms <dan.harms@xrtrading.com>
+# Created: Wednesday, February 12, 2020
+# Version: 1.0
+# Modified Time-stamp: <2020-02-12 13:35:43 dan.harms>
+# Modified by: Dan Harms
+# Keywords: tools
+
+duti -s org.gnu.Emacs com.apple.property-list all
+duti -s org.gnu.Emacs com.apple.xcode.strings-text all
+duti -s org.gnu.Emacs com.netscape.javascript-source all
+duti -s org.gnu.Emacs net.daringfireball.markdown all
+duti -s org.gnu.Emacs public.c-header all
+duti -s org.gnu.Emacs public.c-plus-plus-source all
+duti -s org.gnu.Emacs public.c-source all
+duti -s org.gnu.Emacs public.data all
+duti -s org.gnu.Emacs public.json all
+duti -s org.gnu.Emacs public.objective-c-source all
+duti -s org.gnu.Emacs public.perl-script all
+duti -s org.gnu.Emacs public.plain-text all
+duti -s org.gnu.Emacs public.precompiled-c-header all
+duti -s org.gnu.Emacs public.python-script all
+duti -s org.gnu.Emacs public.ruby-script all
+duti -s org.gnu.Emacs public.shell-script all
+duti -s org.gnu.Emacs public.swift-source all
+duti -s org.gnu.Emacs public.unix-executable all
+duti -s org.gnu.Emacs public.xml all
+duti -s org.gnu.Emacs public.yaml all
+
+# code ends here
