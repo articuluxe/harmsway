eb5de453a3de1f3ff3372e315dc07c86cace126c
Dan Harms <danielrharms@gmail.com>
2020-04-28 08:25:05 -0500

    moved .ssh/config to be site-specific

--- host/rubicon/bin/tar-world.sh
+++ host/rubicon/bin/tar-world.sh
@@ -5,7 +5,7 @@
 # Author: Dan Harms <danielrharms@gmail.com>
 # Created: Friday, May 29, 2015
 # Version: 1.0
-# Modified Time-stamp: <2020-04-28 08:01:52 dharms>
+# Modified Time-stamp: <2020-04-28 08:24:54 dharms>
 # Modified by: Dan Harms
 # Keywords: configuration
 
@@ -41,7 +41,7 @@ if [ -f "$dest" ] ; then
    rm -f "$dest"
 fi
 
-$tar c"$verbose"f "$dest" config doc src .gnupg .fonts .config .terminfo .proviso.d .ssh
+$tar c"$verbose"f "$dest" config doc src .gnupg .fonts .config .terminfo .proviso.d
 $tar u"$verbose"f "$dest" --exclude=*.elc .emacs.d
 $tar u"$verbose"f "$dest" --transform=s%ext%.emacs.d/ext% ext
 $tar u"$verbose"f "$dest" --transform=s/scripts/bin/ scripts
