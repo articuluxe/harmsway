22a0493819c712553817d3cfaa0836f1b66185df
Dan Harms <danielrharms@gmail.com>
2020-08-13 09:13:11 -0500

    remove compilation warning from script

--- scripts/emacscomp.sh
+++ scripts/emacscomp.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 # -*- Mode: sh -*-
 # emacscomp.sh --- byte-compile emacs lisp files
 # Copyright (C) 2015-2017, 2020  Dan Harms (dan.harms)
 # Author: Dan Harms <dan.harms@xrtrading.com>
 # Created: Tuesday, June 16, 2015
 # Version: 1.0
-# Modified Time-stamp: <2020-07-23 11:54:31 dharms>
+# Modified Time-stamp: <2020-08-13 09:12:26 dharms>
 # Keywords: emacs configuration
 
 emacs=$EDITOR
@@ -27,7 +27,7 @@ cmd="(byte-recompile-directory \"$dir\" 0 t)"
 echo "Compiling emacs files in directory $dir..."
 echo -e
 
-$emacs --batch -u "$user" --eval "$cmd" &> "$log"
+$emacs --batch -u "$user" --eval "$cmd" > "$log" 2>&1
 #Done (Total of 292 files compiled, 2 failed in 33 directories)
 grep -i 'error' "$log"
 grep -e '^Done' "$log"
