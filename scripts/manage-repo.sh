adb94962e561792dc4ca91b35a2ba5642aaef15a
Dan Harms <danielrharms@gmail.com>
2021-11-05 13:45:56 -0500

    improve manage-repos script

---
+++ scripts/manage-repo.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+# -*- Mode: sh -*-
+# manage-repo.sh --- clone and update a repo
+# Copyright (C) 2021  Dan Harms (dharms)
+# Author: Dan Harms <enniomore@icloud.com>
+# Created: Friday, November  5, 2021
+# Version: 1.0
+# Modified Time-stamp: <2021-11-05 13:53:56 dharms>
+# Modified by: Dan Harms
+# Keywords: git repo
+
+parent=${1:-$(pwd)}
+
+if [ ! -d "$parent" ]; then
+    echo "!!! $parent is not a valid directory; exiting..."
+    exit 1
+fi
+
+cd "$parent" || exit
+repo=$( basename "$PWD" )
+echo -e
+echo "-*- $repo -*-"
+# clone
+if [ ! -d "$parent/src" ] && [ -f "$parent/clone" ]; then
+    echo "Cloning $repo"
+    ./clone
+fi
+# update
+if [ -d "$parent/src/.git" ] || [ -d "$parent/src/.git-ignore" ]; then
+    if [ -f "update" ]; then
+        updatefile="$parent/update"
+    fi
+    if [ -f "external" ]; then
+        rel=$( cat external )
+        abs=$( _realpath.sh "$rel" )
+        if [ -d "$abs" ]; then
+            echo "Changing path based on ./external to $abs"
+            cd "$abs" || exit 1
+        else
+            echo "!!! $abs did not exist; ignoring ./external"
+            cd "$parent/src" || exit 1
+        fi
+    else
+        cd "$parent/src" || exit 1
+    fi
+    if [ -d "$parent/src/.git" ]; then
+        # only run these commands for a real git repo
+        git fetch --all
+        git stash
+    fi
+    if [ -f "$updatefile" ]; then
+        echo "Running $updatefile..."
+        "$updatefile"
+    fi
+    res=$( git log HEAD..FETCH_HEAD --oneline )
+    if [ -n "$res" ]; then
+        echo -e
+        echo "--- Updating $repo due to the following differences:"
+        echo "$res"
+        echo -e
+        git pull --no-rebase
+    fi
+elif [ -d "$parent/src/.hg" ]; then
+    if [ -f "update" ]; then
+        ./update
+    fi
+    if [ -f "external" ]; then
+        rel=$( cat external )
+        abs=$( _realpath.sh "$rel" )
+        if [ -d "$abs" ]; then
+            cd "$abs" || exit 1
+        else
+            echo "!!! $abs did not exist; ignoring ./external"
+            cd "$parent/src" || exit 1
+        fi
+    else
+        cd "$parent/src" || exit 1
+    fi
+    hg pull
+    hg update
+fi
+# install
+cd "$parent" || exit
+if [ -f "$parent/.ignore_install" ]; then
+    :
+elif [ -f "$parent/install_$(uname -s)" ]; then
+    ./"install_$(uname -s)"
+elif [ -f "$parent/install" ]; then
+    ./install
+fi
+
+
+# code ends here
--- scripts/manage-repos.sh
+++ scripts/manage-repos.sh
@@ -5,7 +5,7 @@
 # Author: Dan Harms <enniomore@icloud.com>
 # Created: Friday, March 17, 2017
 # Version: 1.0
-# Modified Time-stamp: <2021-09-08 17:24:01 dharms>
+# Modified Time-stamp: <2021-11-05 12:15:44 dharms>
 # Modified by: Dan Harms
 # Keywords: git repo
 
@@ -20,49 +20,7 @@ cd "$dir" || exit 1
 
 find "$dir" -type f -name clone | sort | while read -r fname; do
     parent=$( dirname "$fname" )
-    cd "$parent" || exit
-    repo=$( basename "$PWD" )
-    echo -e
-    echo "-*- $repo -*-"
-    # clone
-    if [ ! -d "$parent/src" ] && [ -f "$parent/clone" ]; then
-        echo "Cloning $repo"
-        ./clone
-    fi
-    # update
-    if [ -d "$parent/src/.git" ]; then
-        if [ -f "update" ]; then
-            ./update
-        else
-            cd "$parent/src" || exit
-        fi
-        git fetch --all
-        res=$( git log HEAD..origin --oneline )
-        if [ -n "$res" ]; then
-            echo -e
-            echo "--- Updating $repo due to the following differences:"
-            echo "$res"
-            echo -e
-            git pull --no-rebase
-        fi
-    elif [ -d "$parent/src/.hg" ]; then
-        if [ -f "update" ]; then
-            ./update
-        else
-            cd "$parent/src" || exit
-        fi
-        hg pull
-        hg update
-    fi
-    # install
-    cd "$parent" || exit
-    if [ -f "$parent/.ignore_install" ]; then
-        :
-    elif [ -f "$parent/install_$(uname -s)" ]; then
-        ./"install_$(uname -s)"
-    elif [ -f "$parent/install" ]; then
-        ./install
-    fi
+    manage-repo.sh "$parent"
 done
 
 # code ends here
