5b3be8198b03b49f030dc59c734ec6a997fa6513
Dan Harms <danielrharms@gmail.com>
2022-01-04 11:24:47 -0600

    remove compiler warnings from script

--- scripts/bootstrap-harmsway.sh
+++ scripts/bootstrap-harmsway.sh
@@ -1,26 +1,27 @@
 #!/bin/sh
 # -*- Mode: sh -*-
 # bootstrap-harmsway.sh --- bootstrap harmsway
-# Copyright (C) 2021  Dan Harms (dharms)
+# Copyright (C) 2021-2022  Dan Harms (dharms)
 # Author: Dan Harms <enniomore@icloud.com>
 # Created: Wednesday, March 10, 2021
 # Version: 1.0
-# Modified Time-stamp: <2021-09-01 12:45:22 dharms>
+# Modified Time-stamp: <2022-01-04 11:22:44 dharms>
 # Modified by: Dan Harms
 # Keywords: tools
 
 OS=$(uname)
 
-if [ "$OS" == "Darwin" ]; then
+if [ "$OS" = "Darwin" ]; then
     brew install gnu-tar
     TAR=$(which gtar)
 else
     TAR=$(which tar)
 fi
 
-mkdir -p $HOME/src && cd $HOME/src
+mkdir -p "$HOME/src"
+cd "$HOME/src" || exit 1
 test -d harmsway/.git || git clone --recurse-submodules git@github.com:articuluxe/harmsway.git
-cd harmsway
+cd harmsway || exit 1
 
 rm -f world.tar
 $TAR cf world.tar config doc src .fonts .config .terminfo .proviso.d
@@ -32,7 +33,7 @@ $TAR uf world.tar --transform=s/bash\\/// bash
 $TAR uf world.tar --transform=s%os/$OS\\/%% os/$OS
 
 mv world.tar ~
-cd ~
+cd ~ || exit 1
 $TAR xf world.tar
 
 # code ends here
