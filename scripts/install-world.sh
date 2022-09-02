0bddbb20472dc7004ffc7f3072131701a947722c
Dan Harms <danielrharms@gmail.com>
2022-09-02 12:25:50 -0500

    include git revision in .harmsway.last[tarr|install]ed

--- scripts/install-world.sh
+++ scripts/install-world.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # -*- Mode: sh -*-
 # install-world.sh --- install the world
-# Copyright (C) 2016-2018, 2021  Dan Harms (dharms)
+# Copyright (C) 2016-2018, 2021-2022  Dan Harms (dharms)
 # Author: Dan Harms <danielrharms@gmail.com>
 # Created: Thursday, August 18, 2016
 # Version: 1.0
-# Modified Time-stamp: <2021-02-28 11:05:33 dharms>
+# Modified Time-stamp: <2022-09-02 12:25:32 dharms>
 # Modified by: Dan Harms
 # Keywords: install
 
@@ -17,7 +17,11 @@ if [ ! -d "$dir" ]; then
 fi
 
 cd "$dir" || exit 1
+rev=$( git rev-parse --short HEAD )
+
 date > .harmsway.last-tarred
+echo "$rev" >> .harmsway.last-tarred
+
 tar-world.sh
 
 if [ "$?" != 0 ]; then
@@ -26,7 +30,9 @@ if [ "$?" != 0 ]; then
 fi
 
 mv world.tar ~ && cd ~ && untar-world.sh world.tar
+
 date > .harmsway.last-installed
+echo "$rev" >> .harmsway.last-installed
 
 echo "install-world.sh done; press any key to continue..."
 read -n1 -t5 key
