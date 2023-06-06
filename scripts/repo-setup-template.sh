fbbbc0edad5805627dfd87cc7bd77a01a1c56faf
Dan Harms <danielrharms@gmail.com>
2023-06-06 11:44:25 -0500

    small script and display fixes

--- scripts/repo-setup-template.sh
+++ scripts/repo-setup-template.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 # -*- Mode: sh -*-
 # repo-setup-template.sh --- set up repo
-# Copyright (C) 2016, 2018, 2020  Dan Harms (dan.harms)
+# Copyright (C) 2016, 2018, 2020, 2023  Dan Harms (dan.harms)
 # Author: Dan Harms <enniomore@icloud.com>
 # Created: Tuesday, April 26, 2016
 # Version: 1.0
-# Modified Time-stamp: <2020-07-23 11:55:37 dharms>
+# Modified Time-stamp: <2023-06-06 11:00:58 dharms>
 # Modified by: Dan Harms
 # Keywords: snap repo
 
-cwd=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
+cwd=$(dirname "$(realpath -q "${BASH_SOURCE[0]}")")
 REPO_ROOT=${REPO_ROOT:-$cwd}
 envf=$REPO_ROOT/.repo-env
 
