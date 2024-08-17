9f82f77117415c67985ca40d69c8e54ee4248c71
Dan Harms <danielrharms@gmail.com>
2024-08-16 20:53:47 -0500

    small script tweaks

--- os/Darwin/bin/FinderToDired.sh
+++ os/Darwin/bin/FinderToDired.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cwd=$(dirname "$1")
 osascript -e 'tell application "Emacs" to activate'
-/usr/local/bin/emacsclient -q -s rubicon -n -c --eval "(progn (dired \"$cwd\") (dired-goto-file \"$1\"))"
+/usr/local/bin/emacsclient -q -s "$(hostname -s)" -n -c --eval "(progn (dired \"$cwd\") (dired-goto-file \"$1\"))"
 
 # Automator: New -> Quick Action -> Run Shell Script
 # Workflow receives Current Files or Folders in Finder
--- os/Darwin/bin/OpenFinderFileInEmacs.sh
+++ os/Darwin/bin/OpenFinderFileInEmacs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 osascript -e 'tell application "Emacs" to activate'
-/usr/local/bin/emacsclient -q -s rubicon -n -c --eval "(find-file \"$1\")"
+/usr/local/bin/emacsclient -q -s "$(hostname -s)" -n -c --eval "(find-file \"$1\")"
 
 # Automator: New -> Quick Action -> Run Shell Script
 # Workflow receives Current Files or Folders in Finder
