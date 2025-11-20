12a260cb0810a6875340ca54d1034c89dbd640d6
Dan Harms <danielrharms@gmail.com>
2025-11-19 18:27:31 -0600

    improve bootstrap script

--- scripts/bootstrap-harmsway.sh
+++ scripts/bootstrap-harmsway.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # -*- Mode: sh -*-
 # bootstrap-harmsway.sh --- bootstrap harmsway
-# Copyright (C) 2021-2022, 2024  Dan Harms (dharms)
+# Copyright (C) 2021-2022, 2024-2025  Dan Harms (dharms)
 # Author: Dan Harms <enniomore@icloud.com>
 # Created: Wednesday, March 10, 2021
 # Version: 1.0
-# Modified Time-stamp: <2024-03-21 09:54:45 dharms>
+# Modified Time-stamp: <2025-11-19 18:27:11 dharms>
 # Modified by: Dan Harms
 # Keywords: tools
 
@@ -27,7 +27,7 @@ test -d harmsway/.git || git clone --recurse-submodules https://github.com/artic
 cd harmsway || exit 1
 
 rm -f world.tar
-$TAR cf world.tar config doc src .fonts .config .terminfo .proviso.d
+$TAR cf world.tar config doc src .gnupg .fonts .config .terminfo .proviso.d
 $TAR uf world.tar --exclude=*.elc .emacs.d
 $TAR uf world.tar --transform=s%ext%.emacs.d/ext% ext
 $TAR uf world.tar --transform=s/scripts/bin/ scripts
@@ -38,5 +38,8 @@ $TAR uf world.tar --transform=s%os/$OS\\/%% os/$OS
 mv world.tar ~
 cd ~ || exit 1
 $TAR xf world.tar
+
+# to download:
+# wget https://raw.githubusercontent.com/articuluxe/harmsway/refs/heads/master/scripts/bootstrap-harmsway.sh
 
 # code ends here
