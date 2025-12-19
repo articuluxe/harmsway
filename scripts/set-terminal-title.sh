f07296faa37f8867ed4d2240e4a20b82f03a9cdc
Dan Harms <danielrharms@gmail.com>
2025-12-19 11:18:36 -0600

    update terminal script to bash

--- scripts/set-terminal-title.sh
+++ scripts/set-terminal-title.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 # -*- Mode: sh -*-
 # set-terminal-title.sh --- set terminal title
 # Copyright (C) 2020, 2025  Dan Harms (dharms)
 # Author: Dan Harms <enniomore@icloud.com>
 # Created: Wednesday, May 20, 2020
 # Version: 1.0
-# Modified Time-stamp: <2025-12-18 11:42:12 dharms>
+# Modified Time-stamp: <2025-12-19 11:18:00 dharms>
 # Modified by: Dan Harms
 # Keywords: tools
 
